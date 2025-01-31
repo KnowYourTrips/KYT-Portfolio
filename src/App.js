cac56e56e0fa59769271828bad0b58920b9514dc
krish9711 <krish9711@gmail.com>
2025-01-31 22:57:35 +0530

    Work Section Update
    Updated with a new sliding animation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import './index.css'
-import Home from './components/Home'
-import About from './components/About'
-import Contact from './components/Contact'
-import Footer from './components/Footer'
-import Work from './components/Work'
+import './index.css';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import Work from './components/Work';
+
+import Itinerary from "./assets/Itinerary.png";
+import Explore from "./assets/Explore.png";
+import Social from "./assets/Social.png";
+import Blog from "./assets/Blog.png";
+
+const IMAGES = [Itinerary, Explore, Social, Blog];
 
 function App() {
   return (
@@ -15,12 +22,14 @@ function App() {
         <About />
       </div>
       <div id='work'>
-        <Work />
+        <Work imageUrls={IMAGES} />
       </div>
       <div id='contact'>
         <Contact />
       </div>
-      <Footer />
+      <div id='footer'>
+        <Footer />
+      </div>
     </div>
   )
 }
