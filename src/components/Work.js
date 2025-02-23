bddb87d9e383fe342132ecff8a98bd05312cf702
krish9711 <krish9711@gmail.com>
2025-02-23 23:00:32 +0530

    Updated slider images
    Updated slider images

--- src/components/Work.js
+++ src/components/Work.js
@@ -72,17 +72,3 @@ const Work = ({ imageUrls }) => {
 };
 
 export default Work;
-
-
-/* <div className="work-section-wrapper">
-  <div className="work-section-top">
-    <p className="primary-subheading">Use Cases</p>
-    <h1 className="primary-heading">How It Works</h1>
-    <p className="primary-text">
-      Know Your Trips manages your travel experiences from start to finish.
-    </p>
-  </div>
-  <div className="work-section-bottom">
-    <img src={FlowDiagram} alt="" />
-  </div>
-</div>  */
