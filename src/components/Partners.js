7904fee18a2658a3ac5768308ecfb05d57afc04c
krish9711 <krish9711@gmail.com>
2025-04-01 19:32:03 +0530

    Updated Partner Title
    Title updated from "Our Partners" to "Our Affiliated Partners"

--- src/components/Partners.js
+++ src/components/Partners.js
@@ -6,7 +6,7 @@ import "./Styles/Partners.css";
 const Partners = () => {
   return (
     <div id="partners" className="partners-container">
-      <h2 className="partners-title">Our Partners</h2>
+      <h2 className="partners-title">Our Affiliated Partners</h2>
       <div className="partners-logos">
         <img src={GooglePartner} alt="Partner 1" className="partner-logo" />
         <img src={ViatorPartner} alt="Partner 2" className="partner-logo" />
