e7007eadd2eb7dea4d6e47b1b79a57927b954c7a
krish9711 <krish9711@gmail.com>
2025-04-03 20:55:09 +0530

    Video added
    Added a promo video

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,7 @@
 import React from "react";
 import AboutBackground from "../assets/about-background.png";
-import AboutImage from "../assets/About-Image.jpeg";
+// import AboutImage from "../assets/About-Image.jpeg";
+import AboutVideo from "../assets/About-Video.mp4";
 
 const About = () => {
   return (
@@ -8,8 +9,13 @@ const About = () => {
       <div className="about-background-image-container">
         <img src={AboutBackground} alt="" />
       </div>
-      <div className="about-section-image-container">
-        <img src={AboutImage} alt="" className="homepage-image" />
+      {/* <div className="about-section-image-container"> */}
+      <div className="about-section-video-container">
+        {/* <img src={AboutImage} alt="" className="homepage-image" /> */}
+        <video className="homepage-video" autoPlay loop muted>
+          <source src={AboutVideo} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
       </div>
       <div className="about-section-text-container">
         <p className="primary-subheading">About</p>
