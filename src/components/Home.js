624a3a739f73d0ab989ceaa91b9f8a5449b275d9
krish9711 <krish9711@gmail.com>
2025-04-24 20:44:03 +0530

    Product Hunt Embed
    Added Product Hunt Embed

--- src/components/Home.js
+++ src/components/Home.js
@@ -17,21 +17,30 @@ const Home = () => {
           <p className="primary-text">
             Leave the hassle of organzing your trip to our app. Sit back, relax and enjoy your journey ahead.
           </p>
-          <div className="app-store-logo-container">
-            <a
-              href="https://apps.apple.com/in/app/knowyourtrips/id6742848169" // Replace with your App Store link
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img src={AppStoreLogo} alt="Download on the App Store" className="app-store-logo" />
-            </a>
+          <div className="app-store-and-product-hunt-container">
+            <div className="app-store-logo-container">
+              <a
+                href="https://apps.apple.com/in/app/knowyourtrips/id6742848169"
+                target="_blank"
+                rel="noopener noreferrer"            >
+                <img src={AppStoreLogo} alt="Download on the App Store" className="app-store-logo" />
+              </a>
+            </div>
+            <div className="product-hunt-badge-container">
+              <a href="https://www.producthunt.com/posts/knowyourtrips?embed=true&utm_source=badge-featured&utm_medium=badge&utm_souce=badge-knowyourtrips"
+                target="_blank"
+                rel="noopener noreferrer">
+                <img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=956968&theme=dark&t=1745506618137" alt="KnowYourTrips - Your&#0032;Personal&#0032;Travel&#0032;Assistant&#0032; | Product Hunt" style={{ width: "250px", height: "54px" }} width="250" height="54"
+                className="product-hunt-badge" />
+              </a>
+            </div>
           </div>
         </div>
         <div className="home-image-section">
           <img src={BannerImage} alt="" />
         </div>
       </div>
-    </div>
+    </div >
   );
 };
 
